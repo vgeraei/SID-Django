d6cb75fa006c9fe2336c910195046a8276ca0a16
Vahid Geraeinejad <vahidgn@gmail.com>
2021-08-11 15:58:47 +0430

    deployment changes

--- EmotionDetector/settings.py
+++ EmotionDetector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wdtbe#5=5jd=^rid@$dfye8(d0+c(_t*@%^bvb+p$k@md!^=v_'
+# SECRET_KEY = 'django-insecure-wdtbe#5=5jd=^rid@$dfye8(d0+c(_t*@%^bvb+p$k@md!^=v_'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',  'django-insecure-wdtbe#5=5jd=^rid@$dfye8(d0+c(_t*@%^bvb+p$k@md!^=v_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
